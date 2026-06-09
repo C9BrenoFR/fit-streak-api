90a5c2a34a94fef17e830e6cc72c9d07a6f5c91a
BrenoRMachado <brenomachado.3064@gmail.com>
2026-06-09 20:29:36 -0300

    feat: adiciona método create nas services

--- src/main/java/br/ufjf/fsapi/api/dto/WorkoutDTO.java
+++ src/main/java/br/ufjf/fsapi/api/dto/WorkoutDTO.java
@@ -1,8 +1,11 @@
 package br.ufjf.fsapi.api.dto;
 
+import br.ufjf.fsapi.model.entity.User;
+import br.ufjf.fsapi.model.entity.Workout;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 
 
 @Data
@@ -17,4 +20,9 @@ public class WorkoutDTO {
     //private LocalDate date;
     private Long idUser;
     private Long idCreator;
+
+    public static WorkoutDTO create(Workout workout){
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(workout, WorkoutDTO.class);
+    }
 }
